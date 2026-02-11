ea4a6b8c56edd7a65dfd4304eff86b1501dc23cd
Фёдор Полевой <polevoy@ligat.su>
2026-02-11 16:18:37 +0300

    Тестирование логотипа

--- src/components/sections/home/HeroSection.tsx
+++ src/components/sections/home/HeroSection.tsx
@@ -1,3 +1,4 @@
+import LogoIcon from '@/components/icons/logo.svg'
 import { Section } from '@/components/layout/Section/Section'
 import { CapsuleLink } from '@/components/ui/CapsuleLink/CapsuleLink'
 import { Text } from '@/components/ui/Text/Text'
@@ -7,10 +8,26 @@ import styles from './HeroSection.module.css'
 export const HeroSection = () => {
 	return (
 		<Section>
-			<div className={styles.wrapper}>
-				<Text as={'h1'} size={'5xl'} className={styles.heading}>
-					Pole
-				</Text>
+			<div className={styles.container}>
+				<div className={styles.wrapper}>
+					<Text
+						as={'h1'}
+						size={'5xl'}
+						weight={'semibold'}
+						className={styles.heading}
+					>
+						Po
+					</Text>
+					<LogoIcon className={styles.logo} />
+					<Text
+						as={'h1'}
+						size={'5xl'}
+						weight={'semibold'}
+						className={styles.heading}
+					>
+						le
+					</Text>
+				</div>
 				<CapsuleLink href={'/contacts'}>
 					<Text as={'span'} size={'xxl'} className={styles.headingLink}>
 						Студия
