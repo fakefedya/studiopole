d49fb5f135a538ec7c24c023397b2660ce611c64
Фёдор Полевой <polevoy@ligat.su>
2026-02-17 11:58:06 +0300

    Эксперименты с Hero

--- src/components/layout/Section/Section.tsx
+++ src/components/layout/Section/Section.tsx
@@ -1,11 +1,18 @@
 import { ReactNode } from 'react'
 
+import { cn } from '@/lib/cn'
+
 import styles from './Section.module.css'
 
 interface Props {
 	children: ReactNode
+	className?: 'wide'
 }
 
-export const Section = ({ children }: Props) => {
-	return <section className={styles.section}>{children}</section>
+export const Section = ({ children, className }: Props) => {
+	return (
+		<section className={cn(styles.section, className && styles[className])}>
+			{children}
+		</section>
+	)
 }
--- src/components/sections/home/Hero/Hero.tsx
+++ src/components/sections/home/Hero/Hero.tsx
@@ -1,41 +1,45 @@
 import Image from 'next/image'
 
+import Icon from '@/components/icons/logo.svg'
 import { Section } from '@/components/layout/Section/Section'
-import { CapsuleLink } from '@/components/ui/CapsuleLink/CapsuleLink'
 import { Text } from '@/components/ui/Text/Text'
 
 import styles from './Hero.module.css'
 
 export const Hero = () => {
 	return (
-		<Section>
-			<div className={styles.heroBackground}>
-				<div className={styles.heroBackgroundWrapper}>
-					<Image
-						className={styles.heroBackgroundImage}
-						src={'/images/projects/zolotaya-rosha/cover.jpg'}
-						alt={'opiuj'}
-						width={2768}
-						height={2400}
-					/>
+		<Section className={'wide'}>
+			<div className={styles.root}>
+				<div className={styles.background}>
+					<div className={styles.backgroundWrapper}>
+						<Image
+							className={styles.image}
+							src={'/images/projects/zolotaya-rosha/cover.jpg'}
+							alt={'opiuj'}
+							width={2768}
+							height={2400}
+						/>
+					</div>
 				</div>
-			</div>
-			<div className={styles.container}>
-				<div className={styles.wrapper}>
-					<Text as={'h1'} size={'5xl'} className={styles.heading}>
-						Pole
-					</Text>
+				<div className={styles.heading}>
+					<div className={styles.headingWrapper}>
+						<Text as={'span'} size={'5xl'} weight={'medium'}>
+							P
+						</Text>
+						<div className={styles.headingContainer}>
+							<Text as={'span'} size={'5xl'} weight={'medium'}>
+								O
+							</Text>
+							<Icon />
+							<Text as={'span'} size={'5xl'} weight={'medium'}>
+								L
+							</Text>
+						</div>
+						<Text as={'span'} size={'5xl'} weight={'medium'}>
+							E
+						</Text>
+					</div>
 				</div>
-				<CapsuleLink href={'/contacts'}>
-					<Text
-						as={'span'}
-						size={'xxl'}
-						weight={'medium'}
-						className={styles.headingLink}
-					>
-						Студия
-					</Text>
-				</CapsuleLink>
 			</div>
 		</Section>
 	)
