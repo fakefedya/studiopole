b3a63059c6f0308d5cc64b03f67501973138f7d0
Фёдор Полевой <polevoy@ligat.su>
2026-02-26 10:56:03 +0300

    Замена шрифтов

--- src/components/sections/home/Hero/Hero.tsx
+++ src/components/sections/home/Hero/Hero.tsx
@@ -1,12 +1,8 @@
 import Image from 'next/image'
 
-import IconE from '@/components/icons/iconE.svg'
-import IconL from '@/components/icons/iconL.svg'
-import IconO from '@/components/icons/iconO.svg'
-import IconP from '@/components/icons/iconP.svg'
 import Icon from '@/components/icons/logo.svg'
 import { Section } from '@/components/layout/Section/Section'
-// import { Text } from '@/components/ui/Text/Text'
+import { Text } from '@/components/ui/Text/Text'
 
 import styles from './Hero.module.css'
 
@@ -27,30 +23,25 @@ export const Hero = () => {
 				</div>
 				<div className={styles.heading}>
 					<div className={styles.headingWrapper}>
-						<div className={styles.letterWrapper}>
-							<IconP className={styles.logoIcon} />
-							<IconO className={styles.logoIcon} />
-						</div>
-						{/* <Text as={'span'} size={'5xl'} weight={'medium'}>
-							P
-							</Text> */}
-						<div className={styles.headingContainer}>
-							{/* <Text as={'span'} size={'5xl'} weight={'medium'}>
+						<div className={styles.headingText}>
+							<Text as={'span'} size={'5xl'} weight={'medium'}>
+								P
+							</Text>
+							<Text as={'span'} size={'5xl'} weight={'medium'}>
 								O
-							</Text> */}
+							</Text>
+						</div>
+						<div className={styles.headingContainer}>
 							<Icon className={styles.logo} />
-							{/* <Text as={'span'} size={'5xl'} weight={'medium'}>
+						</div>
+						<div className={styles.headingText}>
+							<Text as={'span'} size={'5xl'} weight={'medium'}>
 								L
-							</Text> */}
+							</Text>
+							<Text as={'span'} size={'5xl'} weight={'medium'}>
+								E
+							</Text>
 						</div>
-						<div className={styles.letterWrapper}>
-							<IconL className={styles.logoIcon} />
-
-							<IconE className={styles.logoIcon} />
-						</div>
-						{/* <Text as={'span'} size={'5xl'} weight={'medium'}>
-							E
-						</Text> */}
 					</div>
 				</div>
 			</div>
